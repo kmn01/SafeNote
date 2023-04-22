bbf54822d43ab0eb6f02e5bc1097e829a11fd649
kmn01 <55681293+kmn01@users.noreply.github.com>
2023-04-22 16:59:17 +0530

    View Note

--- app/src/main/java/com/trial/safenote/ViewNoteActivity.java
+++ app/src/main/java/com/trial/safenote/ViewNoteActivity.java
@@ -5,23 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ViewNoteActivity extends AppCompatActivity {
 
-    FloatingActionButton editnotebutton;
+    private FloatingActionButton editnotebutton;
+    private TextView viewnote_title;
+    private TextView viewnote_content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_note);
 
+        viewnote_title = findViewById(R.id.viewnote_title);
+        viewnote_content = findViewById(R.id.viewnote_content);
         editnotebutton = findViewById(R.id.editnotefab);
+
+        Intent data = getIntent();
+        viewnote_title.setText(data.getStringExtra("title"));
+        viewnote_content.setText(data.getStringExtra("content"));
+
         editnotebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ViewNoteActivity.this, EditNoteActivity.class);
+                Intent intent = new Intent(view.getContext(), EditNoteActivity.class);
+                intent.putExtra("title", data.getStringExtra("title"));
+                intent.putExtra("content", data.getStringExtra("content"));
+                intent.putExtra("noteId", data.getStringExtra("noteId"));
                 startActivity(intent);
             }
         });
